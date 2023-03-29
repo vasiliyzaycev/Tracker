fd36f10b67f7357905eeac4acefccb89288b2512
Vasiliy <vas-zajcev@yandex.ru>
2023-03-29 20:23:33 +0300

    Add asyncTrack

--- Sources/Interface/Tracker.swift
+++ Sources/Interface/Tracker.swift
@@ -8,3 +8,11 @@
 public protocol Tracker {
   func track(_ event: Trackable) async throws
 }
+
+extension Tracker {
+  public func asyncTrack(_ event: Trackable, priority: TaskPriority? = nil) {
+    Task(priority: priority) {
+      try await track(event)
+    }
+  }
+}
